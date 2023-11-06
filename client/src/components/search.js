eb996758c6dece079a1d3bcb1c928bad12f5d3a5
Mariana Fernandes <a92048@alunos.uminho.pt>
2023-11-06 18:17:30 +0000

    dropdown a dar

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,100 +1,120 @@
-import { React, useState, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-function renderTabelaRecursive(tabela) {
-
-    if (!tabela) {
-        return null;
-      }
+function toggleSubItems(e) {
+    const subItems = e.target.nextElementSibling;
+    const parentDropdown = e.target.parentElement;
   
-    return (
-      <div>
-        <h6>{tabela.id} {tabela.desc}</h6>
-        {tabela.nota ? <p>Nota: {tabela.nota}</p> : null}
-        {tabela.valor ? <p>Valor: {tabela.valor}</p> : null}
-        {tabela.refs ? <p>Referência a: {tabela.refs}</p> : null}
-        {tabela.sub && tabela.sub.map((subData) => (
-        <div key={subData.id}>
-          {renderTabelaRecursive(subData)}
-        </div>
-      ))}
+    if (subItems) {
+      if (subItems.classList.contains('show')) {
+        subItems.classList.remove('show');
+        parentDropdown.style.height = 'auto'; // Restaurar a altura original
+      } else {
+        subItems.classList.add('show');
+        parentDropdown.style.height = parentDropdown.style.height + subItems.style.height + 'px'; // Ajustar a altura
+      }
+    }
+  }
+  
+  document.addEventListener('click', function (e) {
+    if (e.target.classList.contains('dropbtn')) {
+      toggleSubItems(e);
+    }
+  });
 
+function renderTabela(tabela, level) {
+    return (
+      <div className={`dropdown-levels dropdown-level-${level}`}>
+          <div className={`dropdown`}>
+            <button className="dropbtn">
+              {tabela['id']}  {tabela['desc']}
+            </button>
+            <div className={`dropdown-content submenu`}>
+              {tabela['nota'] ? <a>Nota: {tabela['nota']}</a> : null}
+              {tabela['valor'] ? <a>Valor: {tabela['valor']}</a> : null}
+              {tabela['refs'] ? <a>Referência a: {tabela['refs']}</a> : null}
+              {tabela.sub ? (tabela.sub.map((subData) => (
+                <a key={subData.id}>{renderTabela(subData, level + 1)}</a>))) : null}
+            </div>
+          </div>
       </div>
     );
   }
-  
 
 function Search() {
-    const baseURL = "http://localhost:3001/tabela/search"; 
+  const baseURL = "http://localhost:3001/tabela/search";
 
-    const [targetId, setTargetId] = useState(""); 
-    const [entry, setEntry] = useState([]);
+  const [targetId, setTargetId] = useState("");
+  const [entry, setEntry] = useState([]);
 
-    const handleInputChange = (event) => {
-        setTargetId(event.target.value);
+  const handleInputChange = (event) => {
+    setTargetId(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    if (targetId) {
+      axios.get(`${baseURL}/${targetId}`).then((response) => {
+        console.log(JSON.stringify(response.data));
+        setEntry(response.data);
+      });
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'Enter') {
+        handleSearchClick(); // Chama a função de pesquisa quando a tecla "Enter" for pressionada
+      }
     };
 
-    const handleSearchClick = () => {
-        if (targetId) {
-            axios.get(`${baseURL}/${targetId}`).then((response) => {
-                console.log(JSON.stringify(response.data));
-                setEntry(response.data);
-            });
-        }
+    // Adicione o event listener quando o componente for montado
+    document.addEventListener('keydown', handleKeyPress);
+
+    // Remova o event listener quando o componente for desmontado
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
     };
+  }, [targetId, handleSearchClick]);
 
-    console.log("ENTRY", entry);
+  console.log("ENTRY", entry);
 
-    return (
-        <div>
-        <div className="search-bar">
-            <input type="text"
-                className="form-control"
-                placeholder="Pesquisa"
-                aria-label="Pesquisa"
-                aria-describedby="addon-wrapping"
-                value={targetId}
-                onChange={handleInputChange}/>
-            <button type="submit" onClick={handleSearchClick}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+  return (
+    <div>
+      <div className="search-bar">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Pesquisa"
+          aria-label="Pesquisa"
+          aria-describedby="addon-wrapping"
+          value={targetId}
+          onChange={handleInputChange}
+        />
+        <button type="submit" onClick={handleSearchClick}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
                     <path d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z" stroke="#525256" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                     <path d="M21 20.9999L16.65 16.6499" stroke="#525256" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                </svg>
-            </button>
-        </div>
-        <div className="result-container">
-            {entry && entry.length > 0 ? (
+            </svg>
+        </button>
+      </div>
+      <div className="result-container">
+        {!entry ? (
+                <p>Dados ainda estão sendo carregados...</p>
+            ) : entry.length > 0 ? (
                 entry.map((item, index) => (
                 <div key={index}>
-                    {renderTabelaRecursive(item)}
-                    <hr></hr>
+                    {renderTabela(item, 1)}
+                    <hr />
                 </div>
                 ))
-            ) : "Não encontrado na tabela."}
-        </div>
-        </div>
-
-        // <div className="search-container">
-        //     <div className="input-container">
-        //         <input
-        //             type="text"
-        //             className="form-control"
-        //             placeholder="Pesquisa"
-        //             aria-label="Pesquisa"
-        //             aria-describedby="addon-wrapping"
-        //             value={targetId}
-        //             onChange={handleInputChange}
-        //         />
-        //         <button onClick={handleSearchClick}>
-        //             Search
-        //         </button>
-        //     </div>
-        //     <div className="result-container">
-        //         {entry ? renderTabelaRecursive(entry) : null}
-        //     </div>
-        // </div>
-    );
+            ) : (
+                <p>Não encontrado na tabela.</p>
+            )}
+      </div>
+    </div>
+  );
 }
+
+
 
 export default Search;
--- client/src/components/tabela.js
+++ client/src/components/tabela.js
@@ -1,67 +1,116 @@
 import { React, useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
+import Dropdown from 'react-dropdown-select';
 
-function renderTabelaRecursive(tabela) {
+
+// function renderTabelaRecursive(tabela) {
+//   return (
+//     <div>
+//       {tabela && Object.keys(tabela).map((key) => (
+//           <div key={key}>
+//             <h6>{tabela[key]['id']} {tabela[key]['desc']}</h6>
+//             {tabela[key]['nota'] ? (
+//               <p>Nota: {tabela[key]['nota']}</p>
+//             ) : (
+//               <></>
+//             )
+//             }
+//             {tabela[key]['valor'] ? (
+//               <p>Valor: {tabela[key]['valor']}</p>
+//             ) : (
+//               <></>
+//             )
+//             }
+//             {tabela[key]['refs'] ? (
+//               <p>Referência a: {tabela[key]['refs']}</p>
+//             ) : (
+//               <></>
+//             )
+//             }
+//             <div>
+//               {renderTabelaRecursive(tabela[key]['sub'])}
+//             </div>
+//           </div>
+//       ))}
+//       </div>
+//   );
+// }
+
+// function renderTabela(tabela) {
+//     if (!tabela || !tabela[0] || !tabela[0].Tabela) {
+//       return <p>Dados ainda estão sendo carregados...</p>;
+//     }
+  
+//     return (
+//       <div>
+//         {Object.keys(tabela[0].Tabela).map((key) => (
+//           <div key={key}>
+//                 <h3>Capítulo {tabela[0].Tabela[key]['id']}: {tabela[0].Tabela[key]['desc']}</h3>
+//                 <hr></hr>
+//                 <div>
+//                   {renderTabelaRecursive(tabela[0].Tabela[key]['sub'])}
+//                 </div>
+//               <hr></hr>
+//             </div>
+//           ))}
+//         </div>
+// );
+//   }
+
+function toggleSubItems(e) {
+  const subItems = e.target.nextElementSibling;
+  const parentDropdown = e.target.parentElement;
+  console.log(parentDropdown);
+  console.log(subItems);
+
+  if (subItems.style.display === 'block') {
+    subItems.style.display = 'none';
+    parentDropdown.style.height = 'auto'; // Restaura a altura original
+  } else {
+    subItems.style.display = 'block';
+    parentDropdown.style.height = parentDropdown.style.height + subItems.style.height + 'px'; // Ajustar a altura
+  }
+}
+
+document.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dropbtn')) {
+    toggleSubItems(e);
+  }
+});
+
+
+function renderTabela(tabela, level) {
   return (
-    <div>
-      {tabela && Object.keys(tabela).map((key) => (
-          <div key={key}>
-            <h6>{tabela[key]['id']} {tabela[key]['desc']}</h6>
-            {tabela[key]['nota'] ? (
-              <p>Nota: {tabela[key]['nota']}</p>
-            ) : (
-              <></>
-            )
-            }
-            {tabela[key]['valor'] ? (
-              <p>Valor: {tabela[key]['valor']}</p>
-            ) : (
-              <></>
-            )
-            }
-            {tabela[key]['refs'] ? (
-              <p>Referência a: {tabela[key]['refs']}</p>
-            ) : (
-              <></>
-            )
-            }
-            <div>
-              {renderTabelaRecursive(tabela[key]['sub'])}
-            </div>
+    <div className={`dropdown-levels dropdown-level-${level}`}>
+      {Object.keys(tabela).map((key) => (
+        <div className={`dropdown`}>
+          <button className="dropbtn">
+            {tabela[key]['id']} {tabela[key]['desc']}
+          </button>
+          <div className={`dropdown-content submenu`}>
+            {tabela[key]['nota'] ? <a>Nota: {tabela[key]['nota']}</a> : null}
+            {tabela[key]['valor'] ? <a>Valor: {tabela[key]['valor']}</a> : null}
+            {tabela[key]['refs'] ? <a>Referência a: {tabela[key]['refs']}</a> : null}
+            {tabela[key]['sub'] ? (
+              <a>{renderTabela(tabela[key]['sub'], level + 1)}</a>
+            ) : null}
           </div>
+        </div>
       ))}
-      </div>
+    </div>
   );
 }
 
-function renderTabela(tabela) {
-    if (!tabela || !tabela[0] || !tabela[0].Tabela) {
-      return <p>Dados ainda estão sendo carregados...</p>;
-    }
-  
-    return (
-      <div>
-        {Object.keys(tabela[0].Tabela).map((key) => (
-          <div key={key}>
-                <h3>Capítulo {tabela[0].Tabela[key]['id']}: {tabela[0].Tabela[key]['desc']}</h3>
-                <hr></hr>
-                <div>
-                  {renderTabelaRecursive(tabela[0].Tabela[key]['sub'])}
-                </div>
-              <hr></hr>
-            </div>
-          ))}
-        </div>
-);
-  }
+
 
 function Tabela() {
 
   const baseURL = "http://localhost:3001/tabela/listTabela";
 
   const [tabela, setTabela] = useState([]);
-  const [loading, setLoading] = useState(true); // Track loading state
+  const [loading, setLoading] = useState(true); 
+  const level = 1;
 
 useEffect(() => {
   const fetchData = async () => {
@@ -83,7 +132,11 @@ useEffect(() => {
 
  
   return (
-    <div>{renderTabela(tabela)}</div>
+    !tabela || !tabela[0] || !tabela[0].Tabela ? (
+      <p>Dados ainda estão sendo carregados...</p>
+    ) : (
+      renderTabela(tabela[0].Tabela, level)
+    )
   );
 }
 
